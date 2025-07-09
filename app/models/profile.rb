809489d4fe414e88ae2e96e4cecc98d0b5fd78c7
Pakapol Tanachaisith <meen260600@gmail.com>
2025-07-09 22:44:21 +0700

    Show user's avatar in the Navbar

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,9 @@ class Profile < ApplicationRecord
   SUPPORTED_AVATAR_FORMATS = [ "image/png", "image/jpeg", "image/webp" ].freeze
   belongs_to :user
 
-  has_one_attached :avatar
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [ 100, 100 ]
+  end
 
   validates_associated :user
   validates :display_name, presence: true
