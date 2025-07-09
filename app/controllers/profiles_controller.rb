c49c7e0201b6514aeb94587a6e71ee21002bcf87
Pakapol Tanachaisith <meen260600@gmail.com>
2025-07-09 19:27:35 +0700

    Add Edit Profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,19 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_profile, only: %i[ show edit ]
 
   def show
-    @profile = Profile.find(params[:id])
     @is_owner = @profile.user == current_user
   end
+
+  def edit
+    if @profile.user != current_user
+      redirect_to root_path, alert: "Unauthorized access."
+    end
+  end
+
+  private
+   def set_profile
+    @profile = Profile.find(params[:id])
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   }
 
   root "pages#home"
-  resources :profiles, only: %i[ show ]
+  resources :profiles, only: %i[ show edit ]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
