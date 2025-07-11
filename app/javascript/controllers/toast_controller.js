d9cd406ed44c7b186cb251800470f59c421fe968
Pakapol Tanachaisith <meen260600@gmail.com>
2025-07-11 19:51:31 +0700

    Add toasts to Application Layout

---
+++ app/javascript/controllers/toast_controller.js
@@ -0,0 +1,8 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="toast"
+export default class extends Controller {
+  dismiss() {
+    this.element.remove();
+  }
+}
