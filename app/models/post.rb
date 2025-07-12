8759f1b4c0ee04f12bb07d8e45c71aaecaef8198
Pakapol Tanachaisith <meen260600@gmail.com>
2025-07-12 21:23:08 +0700

    Add images field to the Post Form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,15 @@
 class Post < ApplicationRecord
+  SUPPORTED_IMAGES_FORMATS = [ "image/png", "image/jpeg", "image/webp" ].freeze
+
   belongs_to :user
   has_one :profile, through: :user
 
+  has_many_attached :images
+
   validates_associated :user
   validates :title, presence: true
+  validates :images,
+            limit: { max: 5 },
+            content_type: SUPPORTED_IMAGES_FORMATS,
+            size: { less_than_or_equal_to: 8.megabytes }
 end
